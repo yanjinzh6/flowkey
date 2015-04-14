bbe114b7932b361ecbf5947dbf009a5692d2963d
YanjinZhu <yanjinzh6@gmail.com>
2015-04-14 18:05:07 +0800

    storageManage.go add type.

--- syncmap/storageManage.go
+++ syncmap/storageManage.go
@@ -1,6 +1,44 @@
 package syncmap
 
-import ()
+import (
+	"errors"
+	. "github.com/yanjinzh6/flowkey/tools"
+)
+
+type storage struct {
+	m       SyncMap
+	name    string
+	mapType int
+}
+
+type Storage interface {
+	MapType() (t int)
+}
 
 type storageManage struct {
+	mainMap Storage
+	readMap []Storage
+}
+
+type StorageManage interface {
+}
+
+var sManage StorageManage
+
+func NewStorage(name string, mapType int) Storage {
+	return &storage{
+		m:       NewSyncMapEnt(),
+		name:    name,
+		mapType: mapType,
+	}
+}
+
+func NewStorageManage(name string, mapType int) StorageManage {
+	if sManage == nil {
+		sManage = storageManage{
+			mainMap: NewStorage(name, mapType),
+			readMap: make([]Storage),
+		}
+	}
+	return &sManage
 }
