4dcb7802f5578c2309016fb506eb10035d56383a
YanjinZhu <yanjinzh6@gmail.com>
2015-04-14 18:05:31 +0800

    util.go add const var.

--- tools/util.go
+++ tools/util.go
@@ -2,6 +2,16 @@ package tools
 
 import (
 	"reflect"
+	"time"
+)
+
+const (
+	STORAGE_MAIN_MAP = iota
+	STORAGE_READ_MAP
+)
+
+const (
+	DEFAULT_DURATION_TIME = time.Minute * 30
 )
 
 /**
@@ -20,5 +30,25 @@ func ChKey(val interface{}) (ok bool) {
 		return rv.IsNil()
 	default:
 		return true
+	}
+}
+
+/**
+ * Check the map key type
+ * @param val key
+ * @return ok if key == nil || key type == Chan, Func, Map, Ptr, Interface, Slice return false
+ * @return t type of err, 1: nil, 2: the map unsupport type
+ */
+func ChKeyType(val interface{}) (ok bool, t int) {
+	if val == nil {
+		return false, 1
+	}
+	rv := reflect.ValueOf(val)
+	k := rv.Type().Kind()
+	switch k {
+	case reflect.Chan, reflect.Func, reflect.Map, reflect.Ptr, reflect.Interface, reflect.Slice:
+		return rv.IsNil(), 2
+	default:
+		return true, 0
 	}
 }
