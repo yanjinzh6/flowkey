6b6523afddd93b31b41824a40073d5d4489f5fda
YanjinZhu <yanjinzh6@gmail.com>
2015-04-30 18:08:18 +0800

    add pprof and print.

--- tools/util.go
+++ tools/util.go
@@ -2,7 +2,13 @@ package tools
 
 import (
 	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
 	"reflect"
+	"runtime"
+	"runtime/pprof"
 	"time"
 )
 
@@ -22,11 +28,16 @@ const (
 
 const (
 	DEFAULT_DURATION_TIME = time.Minute * 30
+	DEFAULT_CLEARUP_TIME  = time.Minute * 5
 )
 
 const (
 	STORAGE_DEFAULT_SIZE         = 10000
 	STORAGE_USAGE_AMOUNT float64 = 0.5
+)
+
+const (
+	SERIALIZATION_FILE_PATH = "../data/map"
 )
 
 var (
@@ -39,6 +50,18 @@ var (
 	StorageNotFindError = errors.New("can not find the name of storage list")
 	ParameterTypeError  = errors.New("Parameter error")
 )
+
+var (
+	Debug            = true
+	cpuProfile       = flag.String("cpuprofile", "", "write cpu profile to file")
+	memProfile       = flag.String("memProfile", "", "write memory profile to file")
+	blockProfile     = flag.String("blockProfile", "", "write block profile to file")
+	cpuProfileRate   = flag.Int("cpuProfileRate", 0, "cpuProfileRate")
+	memProfileRate   = flag.Int("memProfileRate", 0, "memProfileRate")
+	blockProfileRate = flag.Int("blockProfileRate", 0, "blockProfileRate")
+)
+
+type ProfileType string
 
 /**
  * Check the map key type
@@ -77,4 +100,98 @@ func ChKeyType(val interface{}) (ok bool, t int) {
 	default:
 		return true, 0
 	}
+}
+
+func Printf(format string, a ...interface{}) (n int, err error) {
+	if Debug {
+		return fmt.Printf(format, a...)
+	} else {
+		return 0, nil
+	}
+}
+
+func Println(a ...interface{}) (n int, err error) {
+	if Debug {
+		return fmt.Println(a...)
+	} else {
+		return 0, nil
+	}
+}
+
+func StartCPUProfile() {
+	if *cpuProfile != "" {
+		f, err := os.Create(*cpuProfile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Can not create cpu profile output file: %s",
+				err)
+			return
+		}
+		if err := pprof.StartCPUProfile(f); err != nil {
+			fmt.Fprintf(os.Stderr, "Can not start cpu profile: %s", err)
+			f.Close()
+			return
+		}
+	}
+}
+
+func StopCPUProfile() {
+	if *cpuProfile != "" {
+		pprof.StopCPUProfile() // 把记录的概要信息写到已指定的文件
+	}
+}
+
+func StartMemProfile() {
+	if *memProfile != "" && *memProfileRate > 0 {
+		runtime.MemProfileRate = *memProfileRate
+	}
+}
+
+func StopMemProfile() {
+	if *memProfile != "" {
+		f, err := os.Create(*memProfile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Can not create mem profile output file: %s", err)
+			return
+		}
+		if err = pprof.WriteHeapProfile(f); err != nil {
+			fmt.Fprintf(os.Stderr, "Can not write %s: %s", *memProfile, err)
+		}
+		f.Close()
+	}
+}
+
+func StartBlockProfile() {
+	if *blockProfile != "" && *blockProfileRate > 0 {
+		runtime.SetBlockProfileRate(*blockProfileRate)
+	}
+}
+
+func StopBlockProfile() {
+	if *blockProfile != "" && *blockProfileRate >= 0 {
+		f, err := os.Create(*blockProfile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Can not create block profile output file: %s", err)
+			return
+		}
+		if err = pprof.Lookup("block").WriteTo(f, 0); err != nil {
+			fmt.Fprintf(os.Stderr, "Can not write %s: %s", *blockProfile, err)
+		}
+		f.Close()
+	}
+}
+
+func SaveProfile(workDir string, profileName string, ptype ProfileType, debug int) {
+	if profileName == "" {
+		profileName = string(ptype)
+	}
+	profilePath := filepath.Join(workDir, profileName)
+	f, err := os.Create(profilePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can not create profile output file: %s", err)
+		return
+	}
+	if err = pprof.Lookup(string(ptype)).WriteTo(f, debug); err != nil {
+		fmt.Fprintf(os.Stderr, "Can not write %s: %s", profilePath, err)
+	}
+	f.Close()
 }
